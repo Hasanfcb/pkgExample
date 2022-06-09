a9641cdeeb7166541caa073728a31254c54f6044
Documenter.jl <documenter@juliadocs.github.io>
2022-06-09 21:29:20 +0000

    build based on 78c92da

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#pkgExample.jl","page":"pkgExample.jl","title":"pkgExample.jl","text":"","category":"section"},{"location":"","page":"pkgExample.jl","title":"pkgExample.jl","text":"Documentation for pkgExample.jl","category":"page"}]
+}
